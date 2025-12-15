3c89a2254de5b13c4099f61545100f794a6c6709
Jonathan Krall <jonathan.krall@lexicon.se>
2025-12-15 12:02:06 +0100

    Added some annotations to the Entity and updated Database

--- LexiconImdb/Models/Entities/Movie.cs
+++ LexiconImdb/Models/Entities/Movie.cs
@@ -1,9 +1,14 @@
-﻿namespace LexiconImdb.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LexiconImdb.Models.Entities
 {
     public class Movie
     {
         public int Id { get; set; }
+        [StringLength(20)]
         public string Title { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name ="Release date")]
         public DateTime ReleaseDate { get; set; }
         public Genre Genre { get; set; }
         public float Rating { get; set; }
