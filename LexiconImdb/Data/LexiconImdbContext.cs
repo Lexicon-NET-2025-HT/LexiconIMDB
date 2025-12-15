88e3f564d93d0dd947db55385c6b58c1123a3b37
Jonathan Krall <jonathan.krall@lexicon.se>
2025-12-15 13:21:03 +0100

    Added some seeded movies

--- LexiconImdb/Data/LexiconImdbContext.cs
+++ LexiconImdb/Data/LexiconImdbContext.cs
@@ -15,5 +15,21 @@ namespace LexiconImdb.Data
         }
 
         public DbSet<Movie> Movies { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Movie>().HasData(
+             new Movie { Id = 1, Title = "Titanic", Genre = Genre.Drama, ReleaseDate = DateTime.Parse("1997-01-16"), Rating = 7.9F },
+             new Movie { Id = 2, Title = "Scream", Genre = Genre.Horror, ReleaseDate = DateTime.Parse("1997-08-01"), Rating = 7.4F },
+             new Movie { Id = 3, Title = "The Shining", Genre = Genre.Horror, ReleaseDate = DateTime.Parse("1980-09-26"), Rating = 8.4F },
+             new Movie { Id = 4, Title = "300", Genre = Genre.Action, ReleaseDate = DateTime.Parse("2007-04-04"), Rating = 7.6F },
+             new Movie { Id = 5, Title = "Interstellar", Genre = Genre.Drama, ReleaseDate = DateTime.Parse("2014-11-07"), Rating = 8.7F },
+             new Movie { Id = 6, Title = "The Dark Knight", Genre = Genre.Action, ReleaseDate = DateTime.Parse("2008-07-25"), Rating = 9.1F }
+                );
+
+
+        }
     }
 }
--- LexiconImdb/Migrations/20251215121522_SeededSomeMovies.Designer.cs
+++ LexiconImdb/Migrations/20251215121522_SeededSomeMovies.Designer.cs
@@ -4,6 +4,7 @@ using LexiconImdb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LexiconImdb.Migrations
 {
     [DbContext(typeof(LexiconImdbContext))]
-    partial class LexiconImdbContextModelSnapshot : ModelSnapshot
+    [Migration("20251215121522_SeededSomeMovies")]
+    partial class SeededSomeMovies
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
